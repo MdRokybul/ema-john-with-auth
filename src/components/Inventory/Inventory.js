c5ab7fa193bec2ea6befa00ddc93402e694efd41
MdRokybul <mdrokybulhasan40@gmail.com>
2020-09-30 20:02:15 +0600

    50.3 done

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import fakeData from '../../fakeData';
 
 const Inventory = () => {
+    const handleAddProduct = () => {
+        fetch('http://localhost:5000/addProduct', {
+            method: 'POST',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify(fakeData)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
     return (
         <div>
-            <h1>Inventory coming soon...</h1>
+            <button onClick={handleAddProduct}>Add Product</button>
         </div>
     );
 };
